3e6302c92b34c2d8b24d4de2a12a018660b25dfb
Purnoor Sharma <140926362+noorps@users.noreply.github.com>
2026-07-17 10:02:21 -0500

    fixed scheduled merge

--- sql/02_refresh_table_and_export.sql
+++ sql/02_refresh_table_and_export.sql
@@ -5,7 +5,10 @@ USING (
     release_note_type,
     description,
     published_at,
-    'https://cloud.google.com/release-notes' AS source_url,
+    CONCAT(
+      'https://docs.cloud.google.com/release-notes#',
+      FORMAT_DATE('%B_%d_%Y', DATE(published_at))
+    ) AS source_url,
     TO_HEX(SHA256(CONCAT(product_name, CAST(published_at AS STRING), description))) AS row_id
   FROM `bigquery-public-data.google_cloud_release_notes.release_notes`
 ) AS source
